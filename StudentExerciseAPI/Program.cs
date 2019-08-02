8b6ac76386911b98f717c090526d9a5789c5ce9c
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-08-02 15:15:15 -0500

    Initial commit on part 5

---
+++ StudentExerciseAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentExerciseAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
